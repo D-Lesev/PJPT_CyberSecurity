7bbf3677b802414751c37cf5090c5d20f4e5018e
D-Lesev <106137299+D-Lesev@users.noreply.github.com>
2023-10-05 16:08:39 +0300

    Domain finder based on assetfinder

---
+++ assetfinder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+This script is using the already installed assetfinder script.
+Based on the result it searches for only result which have the given domain url.
+"""
+
+import subprocess
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", dest="url", help="Enter valid url (tesla.com)", required=True)
+parsed_args = parser.parse_args()
+
+output = subprocess.Popen(["assetfinder", parsed_args.url], stdout=subprocess.PIPE).communicate()[0]
+
+output = output.decode("utf-8")
+name_of_file = parsed_args.url
+
+with open(f"{name_of_file}.txt", "w") as f:
+    output_list = output.replace("\n", ":").split(":")
+
+    for url in output_list:
+        if parsed_args.url in url:
+            f.write(url + "\n")
+
